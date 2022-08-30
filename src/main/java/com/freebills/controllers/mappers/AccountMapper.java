70db4ad36ac193b3582169b03b598b113aae3a93
claudiney <claudineycj@gmail.com>
2022-08-30 01:42:07 -0300

    creating account end point

--- src/main/java/com/freebills/controllers/mappers/AccountMapper.java
+++ src/main/java/com/freebills/controllers/mappers/AccountMapper.java
@@ -6,15 +6,20 @@ import com.freebills.controllers.dtos.requests.AccountPutRequestDTO;
 import com.freebills.controllers.dtos.responses.AccountResponseDTO;
 import com.freebills.domains.Account;
 import com.freebills.gateways.AccountGateway;
+import com.freebills.gateways.UserGateway;
 import org.mapstruct.*;
 
-@Mapper(componentModel = "spring",uses = {AccountGateway.class})
+import java.util.List;
+
+@Mapper(componentModel = "spring",uses = {UserGateway.class},unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AccountMapper {
 
     @Mapping(source = "userId",target = "user")
     Account toDomain(AccountPostRequestDTO accountPostRequestDTO);
 
     AccountResponseDTO fromDomain(Account account);
+
+    List<AccountResponseDTO> fromDomainList(List<Account> accounts);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Account updateAccountFromDto(AccountPutRequestDTO accountPutRequestDTO, @MappingTarget Account account);
---
+++ src/main/java/com/freebills/usecases/UpdateAccount.java
@@ -0,0 +1,14 @@
+package com.freebills.usecases;
+
+import com.freebills.domains.Account;
+import com.freebills.gateways.AccountGateway;
+import org.springframework.stereotype.Component;
+
+@Component
+public record UpdateAccount(AccountGateway accountGateway) {
+
+
+    public Account update(final Account account){
+        return accountGateway.update(account);
+    }
+}
