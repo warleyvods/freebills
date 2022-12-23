2929b498e23c5deebe144f3ab41e5cb65b2beeda
Warley <warleyvods@gmail.com>
2022-12-22 22:02:05 -0300

    fix consulta transactionRepository

--- src/main/java/com/freebills/usecases/Dashboard.java
+++ src/main/java/com/freebills/usecases/Dashboard.java
@@ -9,14 +9,14 @@ import com.freebills.domains.enums.TransactionType;
 import com.freebills.gateways.AccountGateway;
 import com.freebills.gateways.TransactionGateway;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
 
-import static java.util.stream.Collectors.*;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.reducing;
 
 @Component
 @AllArgsConstructor
@@ -25,35 +25,29 @@ public class Dashboard {
     private final AccountGateway accountGateway;
     private final TransactionGateway transactionGateway;
 
-    public DashboardResponseDTO getTotalDashboard(final String login, final Integer month, final Integer year) {
-        final var byUserDateFilter = getByUserDateFilter(login, month, year).getContent();
-        final var results = byUserDateFilter.parallelStream()
-                .collect(Collectors.groupingBy(Transaction::getTransactionType, Collectors.reducing(BigDecimal.ZERO, Transaction::getAmount, BigDecimal::add)));
-        final var totalRevenueMonth = results.getOrDefault(TransactionType.REVENUE, BigDecimal.ZERO);
-        final var totalExpenseMonth = results.getOrDefault(TransactionType.EXPENSE, BigDecimal.ZERO);
-        return new DashboardResponseDTO(getTotalValue(login), totalRevenueMonth, totalExpenseMonth,  new BigDecimal(0));
+    public DashboardResponseDTO getTotalDashboard(String login, Integer month, Integer year) {
+        List<Transaction> transactions = getTransactionsByUserDateFilter(login, month, year);
+        BigDecimal totalRevenue = getTotalAmountByType(transactions, TransactionType.REVENUE);
+        BigDecimal totalExpense = getTotalAmountByType(transactions, TransactionType.EXPENSE);
+        return new DashboardResponseDTO(getTotalValue(login), totalRevenue, totalExpense, BigDecimal.ZERO);
     }
 
-    public DashboardExpenseResponseDTO getDashboardExpense(final String login, final Integer month, final Integer year) {
-        final var byUserDateFilter = getByUserDateFilter(login, month, year).getContent();
-        final var results = byUserDateFilter.parallelStream()
-                .filter(transaction -> transaction.getTransactionType() == TransactionType.EXPENSE)
-                .collect(Collectors.groupingBy(Transaction::isPaid, Collectors.reducing(BigDecimal.ZERO, Transaction::getAmount, BigDecimal::add)));
-        final var totalExpensePending = results.getOrDefault(false, BigDecimal.ZERO);
-        final var totalExpenseReciveid = results.getOrDefault(true, BigDecimal.ZERO);
-        final var total = totalExpensePending.add(totalExpenseReciveid);
-        return new DashboardExpenseResponseDTO(getTotalValue(login), totalExpensePending, totalExpenseReciveid,  total);
+    public DashboardExpenseResponseDTO getDashboardExpense(String login, Integer month, Integer year) {
+        List<Transaction> transactions = getTransactionsByUserDateFilter(login, month, year);
+        Map<Boolean, BigDecimal> amountsByPaidStatus = getAmountsByTypeAndPaidStatus(transactions, TransactionType.EXPENSE);
+        BigDecimal totalExpensePending = amountsByPaidStatus.getOrDefault(false, BigDecimal.ZERO);
+        BigDecimal totalExpenseReceived = amountsByPaidStatus.getOrDefault(true, BigDecimal.ZERO);
+        BigDecimal totalExpense = totalExpensePending.add(totalExpenseReceived);
+        return new DashboardExpenseResponseDTO(getTotalValue(login), totalExpensePending, totalExpenseReceived, totalExpense);
     }
 
-    public DashboardRevenueResponseDTO getDashboardRevenue(final String login, final Integer month, final Integer year) {
-        final var byUserDateFilter = getByUserDateFilter(login, month, year).getContent();
-        final var results = byUserDateFilter.parallelStream()
-                .filter(transaction -> transaction.getTransactionType() == TransactionType.REVENUE)
-                .collect(Collectors.groupingBy(Transaction::isPaid, Collectors.reducing(BigDecimal.ZERO, Transaction::getAmount, BigDecimal::add)));
-        final var totalRevenuePending = results.getOrDefault(false, BigDecimal.ZERO);
-        final var totalRevenueReceived = results.getOrDefault(true, BigDecimal.ZERO);
-        final var total = totalRevenuePending.add(totalRevenueReceived);
-        return new DashboardRevenueResponseDTO(getTotalValue(login), totalRevenuePending, totalRevenueReceived, total);
+    public DashboardRevenueResponseDTO getDashboardRevenue(String login, Integer month, Integer year) {
+        List<Transaction> transactions = getTransactionsByUserDateFilter(login, month, year);
+        Map<Boolean, BigDecimal> amountsByPaidStatus = getAmountsByTypeAndPaidStatus(transactions, TransactionType.REVENUE);
+        BigDecimal totalRevenuePending = amountsByPaidStatus.getOrDefault(false, BigDecimal.ZERO);
+        BigDecimal totalRevenueReceived = amountsByPaidStatus.getOrDefault(true, BigDecimal.ZERO);
+        BigDecimal totalRevenue = totalRevenuePending.add(totalRevenueReceived);
+        return new DashboardRevenueResponseDTO(getTotalValue(login), totalRevenuePending, totalRevenueReceived, totalRevenue);
     }
 
     private BigDecimal getTotalValue(String login) {
@@ -62,10 +56,23 @@ public class Dashboard {
                 .filter(Account::isDashboard)
                 .map(Account::getAmount)
                 .reduce(BigDecimal::add)
-                .orElse(new BigDecimal(0));
+                .orElse(BigDecimal.ZERO);
     }
 
-    private Page<Transaction> getByUserDateFilter(String login, Integer month, Integer year) {
-        return transactionGateway.findByUserDateFilter(login, month, year, null, null, null);
+    private List<Transaction> getTransactionsByUserDateFilter(String login, Integer month, Integer year) {
+        return transactionGateway.findByUserDateFilter(login, month, year, null, null, null).getContent();
+    }
+
+    private BigDecimal getTotalAmountByType(List<Transaction> transactions, TransactionType type) {
+        return transactions.stream()
+                .filter(t -> t.getTransactionType() == type)
+                .map(Transaction::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private Map<Boolean, BigDecimal> getAmountsByTypeAndPaidStatus(List<Transaction> transactions, TransactionType type) {
+        return transactions.stream()
+                .filter(transaction -> transaction.getTransactionType() == type)
+                .collect(groupingBy(Transaction::isPaid, reducing(BigDecimal.ZERO, Transaction::getAmount, BigDecimal::add)));
     }
 }
