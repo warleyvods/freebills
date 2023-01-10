ea977842cf60fddc02ef87819370ef51eb88b759
Warley <warleyvods@gmail.com>
2023-01-10 19:43:06 -0300

    fix data.

--- src/main/java/com/freebills/controllers/dtos/responses/TransactionResponseDTO.java
+++ src/main/java/com/freebills/controllers/dtos/responses/TransactionResponseDTO.java
@@ -1,14 +1,11 @@
 package com.freebills.controllers.dtos.responses;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.time.LocalDate;
 
 public record TransactionResponseDTO(
         Long id,
         Double amount,
         boolean paid,
-        @JsonFormat(pattern="dd-MM-yyyy")
         LocalDate date,
         String description,
         String transactionType,
