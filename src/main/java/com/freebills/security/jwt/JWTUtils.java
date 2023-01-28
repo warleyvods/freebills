518a610539a05db45ef86819eb1e1d083e6eb466
Warley <warleyvods@gmail.com>
2023-01-28 14:47:34 -0300

    imports

--- src/main/java/com/freebills/security/jwt/JWTUtils.java
+++ src/main/java/com/freebills/security/jwt/JWTUtils.java
@@ -4,6 +4,8 @@ import com.freebills.domains.User;
 import com.freebills.repositories.UserRepository;
 import com.freebills.security.services.UserDetailsImpl;
 import io.jsonwebtoken.*;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +15,6 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.WebUtils;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Optional;
