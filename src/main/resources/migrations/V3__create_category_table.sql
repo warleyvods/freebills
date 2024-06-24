85d5667a69054d5259032295e0fa3e2574d17626
Warley <warleyvods@gmail.com>
2024-06-24 16:50:53 -0300

    changes table

--- src/main/resources/migrations/V3__create_category_table.sql
+++ src/main/resources/migrations/V3__create_category_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE categories
     created_at    TIMESTAMP(6) NOT NULL,
     updated_at    TIMESTAMP(6) NOT NULL,
     category_type VARCHAR(50)  NOT NULL,
-    user_id       BIGSERIAL CONSTRAINT fk_user_category_id REFERENCES users (id)
+    user_id       BIGINT CONSTRAINT fk_user_category_id REFERENCES users (id)
 );
 
-ALTER TABLE transactions ADD COLUMN category_id BIGSERIAL CONSTRAINT fk_category_transaction_id REFERENCES categories (id)
+ALTER TABLE transactions ADD COLUMN category_id BIGINT;
