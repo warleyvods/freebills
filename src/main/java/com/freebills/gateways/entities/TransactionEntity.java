57d5cd1f3ad980a911d8cd7c97fdd47ae6c90e44
Warley <warleyvods@gmail.com>
2025-01-05 19:16:15 -0300

    add observation field

--- src/main/java/com/freebills/gateways/entities/TransactionEntity.java
+++ src/main/java/com/freebills/gateways/entities/TransactionEntity.java
@@ -16,7 +16,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
